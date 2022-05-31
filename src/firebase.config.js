966bb6d66dc30e20e9d4575b8e6b9f5f0c1a0652
MrGultekin <gultekin.de@gmail.com>
2022-05-31 08:20:39 +0200

    firebase.config.js added.

---
+++ src/firebase.config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDRJ_wefmZjqEIybuYj4fdvCsJM-2gPo0g",
+    authDomain: "dream-home-finder-c5302.firebaseapp.com",
+    projectId: "dream-home-finder-c5302",
+    storageBucket: "dream-home-finder-c5302.appspot.com",
+    messagingSenderId: "870731730567",
+    appId: "1:870731730567:web:928d7cd7a5795f92143222"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
