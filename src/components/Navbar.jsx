951158b9128a891e0f65493f53ff7fe6933df068
MrGultekin <gultekin.de@gmail.com>
2022-06-01 15:16:43 +0200

    Sign In done.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,3 @@
-// import { useNavigate, useLocation } from 'react-router-dom';
 import {ReactComponent as OfferIcon} from '../assets/svg/localOfferIcon.svg';
 import {ReactComponent as ExploreIcon} from '../assets/svg/exploreIcon.svg';
 import {ReactComponent as PersonOutlineIcon} from '../assets/svg/personOutlineIcon.svg';
