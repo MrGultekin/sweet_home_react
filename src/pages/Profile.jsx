48347403e8823694ef590ff9a86d52f632e44dd0
MrGultekin <gultekin.de@gmail.com>
2022-06-02 13:37:01 +0200

    Profile page displays username.

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,17 @@
+import {  useEffect,useState } from 'react'
+import { getAuth } from 'firebase/auth'
+
 
 const Profile = () => {
+    const [user,setUser] = useState(null);
+    const auth = getAuth();
+
+    useEffect(() =>{
+       setUser(auth.currentUser)
+    },[]);
+
     return (
-        <div>
-            <h1>Profile</h1>
-        </div>
+        user ? <h1>{user.displayName}</h1> : 'Not Logged in'
     )
 }
 export default Profile;
