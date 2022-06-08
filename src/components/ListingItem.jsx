e8e189e3e6b8ad97e143162d630ead88987f6fce
MrGultekin <gultekin.de@gmail.com>
2022-06-08 14:03:27 +0200

    Fetch the Listing and Offer page  work

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -5,12 +5,12 @@ import bedIcon from '../assets/svg/bedIcon.svg'
 import bathtubIcon from '../assets/svg/bathtubIcon.svg'
 
 
-const ListingItem = ({listing, id}) => {
+const ListingItem = ({listing, id,onDelete}) => {
     return (
         <li className="categoryListing">
             <Link to={`/category/${listing.type}/${id}`} className='categoryListingLink'>
 
-                <img src={listing.imgUrls[0]} alt={listing.name} className='categoryListingImg'/>
+                <img src={listing.imageUrls[0]} alt={listing.name} className='categoryListingImg'/>
 
                 <div className="categoryListingDetails">
                     <p className="categoryListingLocation"> {listing.location}</p>
@@ -18,10 +18,39 @@ const ListingItem = ({listing, id}) => {
 
                     <p className="categoryListingPrice">
 
-                                €{listing.offer ? listing.discountedPrice :listing.regularPrice}
+                        €{listing.offer
+                        ? listing.discountedPrice
+                            .toString()
+                            .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+                        : listing.regularPrice
+                            .toString()
+                            .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+                        {listing.type === 'rent' && ' / Month'}
                     </p>
+
+                    <div className="categoryListingInfoDiv">
+                        <img src={bedIcon} alt='bed'/>
+                        <p className="categoryListingInfoText">
+                            { listing.bedrooms > 1 ? `${listing.bedrooms} Bedrooms` :'1 Bedroom'}
+                        </p>
+
+                        <img src={bathtubIcon} alt='bath' />
+                        <p className='categoryListingInfoText'>
+                            {listing.bathrooms > 1
+                                ? `${listing.bathrooms} Bathrooms`
+                                : '1 Bathroom'}
+                        </p>
+
+                    </div>
                 </div>
             </Link>
+            {onDelete && (
+                <DeleteIcon
+                    className='removeIcon'
+                    fill='rgb(231, 76,60)'
+                    onClick={() => onDelete(listing.id, listing.name)}
+                />
+            )}
         </li>
     )
 }
--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,14 +1,10 @@
 import {useEffect, useState} from 'react'
 import {useParams} from 'react-router-dom'
-import {
-    collection, getDocs, query, where, orderBy,
-    limit, startAfter,
-} from 'firebase/firestore'
+import {collection, getDocs, query, where, orderBy, limit, startAfter,} from 'firebase/firestore'
 import {db} from '../firebase.config'
 import {toast} from 'react-toastify'
 import Spinner from '../components/Spinner'
 import ListingItem from '../components/ListingItem'
-import(ListingItem)
 
 
 
@@ -30,7 +26,6 @@ const Category = () => {
                     listingsRef,
                     where('type', '==', params.categoryName),
                     orderBy('timestamp', 'desc'),
-                    startAfter(lastFetchedListing),
                     limit(10)
                 )
 
@@ -70,11 +65,11 @@ const Category = () => {
                        <ul className="categoryListings">
                            {listings.map((listing)=>(
                                // <h3 key={listing.id}>{listing.data.name}</h3>
-                               <ListingItem
-                                   listing={listing.data}
-                                   id={listing.id}
-                                   key={listing.id}
-                               />
+                                 <ListingItem
+                                      listing={listing.data}
+                                      id={listing.id}
+                                     key={listing.id}
+                                 />
                            ))}
                        </ul>
                     </main>
