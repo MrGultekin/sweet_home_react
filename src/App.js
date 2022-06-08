6c2a2cf160b824d62d989ea4a56d936b2c0f80ae
MrGultekin <gultekin.de@gmail.com>
2022-06-08 15:45:50 +0200

    CreateListing Page works

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import {Route, BrowserRouter as  Router, Routes} from "react-router-dom";
-
 import{ToastContainer} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css'
 import Explore from "./pages/Explore";
-import ForgotPassword from "./pages/ForgotPassword";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
@@ -11,6 +9,9 @@ import Offers from "./pages/Offers";
 import Category from "./pages/Category";
 import Navbar from "./components/Navbar";
 import PrivateRoute from "./components/PrivateRoute";
+import ForgotPassword from "./pages/ForgotPassword";
+import CreateListing from "./pages/CreateListing";
+
 
 function App() {
     return (
@@ -27,6 +28,7 @@ function App() {
                     <Route path='/sign-in' element={<SignIn/>}/>
                     <Route path='/sign-up' element={<SignUp/>}/>
                     <Route path='/forgot-password' element={<ForgotPassword/>}/>
+                    <Route path='/create-listing' element={<CreateListing/>}/>
                 </Routes>
                 <Navbar/>
             </Router>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -5,6 +5,9 @@ import {db} from '../firebase.config'
 import {useNavigate, Link} from "react-router-dom";
 import {toast} from "react-toastify";
 
+import arrowRight from '../assets/svg/keyboardArrowRightIcon.svg'
+import homeIcon from '../assets/svg/homeIcon.svg'
+
 
 const Profile = () => {
     const auth = getAuth();
@@ -74,6 +77,13 @@ const Profile = () => {
                             : 'profileEmailActive'} disabled={!changeDetails} value={email} onChange={onChange}/>
                     </form>
                 </div>
+
+                <Link to='/create-listing' className='createListing'>
+                    <img src={homeIcon} alt='home' />
+                    <p>Sell or rent your home</p>
+                    <img src={arrowRight} alt='arrow right' />
+                </Link>
+
             </main>
         </div>
     )
